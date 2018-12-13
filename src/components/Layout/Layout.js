778e7ad4a4a5e76d476d348014470edd90ac20d7
Archaeologist03 <omniiknight@hotmail.com>
2018-12-13 18:58:12 +0100

    Initing repo. 'Starting burger component'

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Auxy from '../../hoc/Auxy';
+import classes from './Layout.module.css'
+
+
+const Layout = props => (
+  <Auxy>
+    <div>Toolbar, SideDrawer. Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Auxy>
+);
+
+
+export default Layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import Auxy from '../../hoc/Auxy'
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Auxy>
+        <Burger />
+        <div>Build Controls</div>
+      </Auxy>
+    );
+  }
+}
+
+export default BurgerBuilder;
