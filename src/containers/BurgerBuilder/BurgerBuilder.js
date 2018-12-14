9a075ee22730d29f96291ee91da25e907500dbf4
Ilija <omniiknight@hotmail.com>
2018-12-14 14:07:23 +0100

    calc and outputing ingredients dynamically. 119

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,22 @@ import Auxy from '../../hoc/Auxy'
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    }
+  }
+
+
   render() {
     return (
       <Auxy>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Auxy>
     );
