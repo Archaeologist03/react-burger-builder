5f348aa4db75015029f63840f552ade62040ff5c
Ilija <omniiknight@hotmail.com>
2019-02-07 12:52:42 +0100

    Getting token from backend. Axios post request. Debugged 400 failed req(missed .value in Auth email/pass obj).

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -80,7 +80,7 @@ export class Auth extends Component {
 
   submitHandler = event => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email, this.state.controls.password);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
   }
 
   render() {
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,25 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    console.log(email, password);
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+
+    axios
+      .post(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDL0RIOO9zdfTu81q3cS60wW76CzrZ62jA',
+        authData,
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
