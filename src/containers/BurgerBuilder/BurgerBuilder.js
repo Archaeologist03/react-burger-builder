f6d1723313b081310927375cfd6b5b8c0f15f3dc
Ilija <omniiknight@hotmail.com>
2019-02-10 23:55:45 +0100

    Redirecting users to checkout page, peristing auth state to local storage. 324.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,6 +33,7 @@ class BurgerBuilder extends Component {
     if (this.props.isAuthenticated) {
       this.setState({ purchasing: true });
     } else {
+      this.props.onSetRedirectPath('/checkout');
       this.props.history.push('/auth');
     }
   };
@@ -112,6 +113,7 @@ const mapDispatchToProps = dispatch => {
     onIngredientRemoved: ingName => dispatch(actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
     onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetRedirectPath: path => dispatch(actions.setAuthRedirectPath(path)),
   };
 };
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,19 +24,22 @@ export const authFail = error => {
 };
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
+
   return {
     type: actionTypes.AUTH_LOGOUT,
-  }
-}
+  };
+};
 
-export const checkAuthTimeout = (expirationTime) => {
-  return dispatch  => { 
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
     setTimeout(() => {
-      dispatch(logout())
-
-    }, (expirationTime * 1000));
-  }
-}
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
 
 export const auth = (email, password, isSignup) => {
   return dispatch => {
@@ -60,6 +63,14 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then(response => {
         console.log(response);
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000,
+        );
+
+        localStorage.setItem('token', response.data.idToken);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
+
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -67,5 +78,34 @@ export const auth = (email, password, isSignup) => {
         console.log(err);
         dispatch(authFail(err.response.data.error));
       });
+  };
+};
+
+export const setAuthRedirectPath = path => {
+  return {
+    type: actionTypes.SET_AUTH_REDIRECT_PATH,
+    path: path,
+  };
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            ((expirationDate.getTime() - new Date().getTime()) / 1000),
+          ),
+        );
+      }
+    }
   };
 };
